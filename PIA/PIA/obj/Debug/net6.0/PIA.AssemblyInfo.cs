3fcd2ddee1d6d2db7a5bad38d2eeacb6c5e9093b
juanfra109 <juanfranciscobebe@hotmail.com>
2025-05-20 00:09:21 -0600

    pequeños cambios inicio y login

--- PIA/PIA/obj/Debug/net6.0/PIA.AssemblyInfo.cs
+++ PIA/PIA/obj/Debug/net6.0/PIA.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4958496449e04df352146b2c466a54ef7e92b31d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e4f578aaa6e50fc8fa75b30b619b2e74cfdcb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
