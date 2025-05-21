94d822d5460f741b1968f60861f3910c77eb7821
juanfra109 <juanfranciscobebe@hotmail.com>
2025-05-21 04:01:35 -0600

    catalogo y detalle catalogo diseño

--- PIA/PIA/obj/Debug/net6.0/PIA.AssemblyInfo.cs
+++ PIA/PIA/obj/Debug/net6.0/PIA.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085ec6ee470b67c5c61a50e08ceb9861300e7c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca5f3c2ab79d071dc52c79eb986bc32d4b4b5ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
