d183ec079408d43f2ed5dc04f1e7df8d97f868e6
juanfra109 <juanfranciscobebe@hotmail.com>
2025-05-21 05:17:04 -0600

    index admin completo

--- PIA/PIA/obj/Debug/net6.0/PIA.AssemblyInfo.cs
+++ PIA/PIA/obj/Debug/net6.0/PIA.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72250e11eb0ea64331838e0efe30f60ae9c5bee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e37c5a61f04aa98ae3ee100449830a0247e12f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
